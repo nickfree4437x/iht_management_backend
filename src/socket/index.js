6a026d15434f02c68eb47a5785c671376cb5db6f
nickfree4437x <146987759+nickfree4437x@users.noreply.github.com>
2026-04-01 02:15:06 +0530

    Fix CORS origin URL by removing trailing slash

--- src/socket/index.js
+++ src/socket/index.js
@@ -5,7 +5,7 @@ let io;
 export const initSocket = (server) => {
   io = new Server(server, {
     cors: {
-      origin: "https://iht-management-frontend.vercel.app/",
+      origin: "https://iht-management-frontend.vercel.app",
       methods: ["GET", "POST"],
       credentials: true,
     },
