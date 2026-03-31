fcbd83312da607ef95f155a60c893f30397fd11f
nickfree4437x <146987759+nickfree4437x@users.noreply.github.com>
2026-04-01 01:44:21 +0530

    Update CORS origin for socket server

--- src/socket/index.js
+++ src/socket/index.js
@@ -5,7 +5,7 @@ let io;
 export const initSocket = (server) => {
   io = new Server(server, {
     cors: {
-      origin: "http://localhost:5173",
+      origin: "http://iht-management-frontend.vercel.app",
       methods: ["GET", "POST"],
       credentials: true,
     },
